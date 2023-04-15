23a4d3b3de96c9e49647295ac337e1e532024bc2
Nuradin <37750486+nuradinsultan@users.noreply.github.com>
2023-04-15 19:50:01 +0300

    Create mainPage.js

---
+++ PersonalWebsiteTemplate/mainPage.js
@@ -0,0 +1,17 @@
+/*
+    Your Name's Personal Website
+    Copyright 2022 (C) Your Name
+    Javascript File
+    07/24/2022
+*/
+
+
+
+
+/*Prepare the page for start up*/
+function startUpPage() {
+    console.log("Page loaded.")
+}
+
+/*Fire off functions*/
+window.onload = startUpPage;
